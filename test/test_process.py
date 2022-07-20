2b64608af36dbfb8b6cee6f36c59069979b1c3d2
RagingTiger <yourmom@example.com>
2022-07-20 07:11:58 -0400

    Refactoring to pytest syntax

--- test/test_process.py
+++ test/test_process.py
@@ -1,24 +1,15 @@
-import unittest
-
-# custom libs
 from packages import process
 
 
 # tests
-class TestProcess(unittest.TestCase):
+def test_queue():
+    '''Make sure multiprocessing.Queue works as it should'''
+    # run deploy func on simple builtin pow func
+    results = process.deploy(
+        pow,
+        base=2,
+        exp=2,
+    )
 
-    def test_queue(self):
-        '''Make sure multiprocessing.Queue works as it should'''
-        # run deploy func on simple builtin pow func
-        results = process.deploy(
-            pow,
-            base=2,
-            exp=2,
-        )
-
-        # confirm returned results match
-        self.assertEqual(results, pow(2, 2))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    # confirm returned results match
+    assert results == pow(2, 2)
