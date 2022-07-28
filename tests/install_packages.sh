a85f73a1e3b00866ae17fff04715a830c0f1a292
RagingTiger <yourmom@example.com>
2022-07-28 10:47:38 -0400

    Replaced deprecated apt-key add command

--- tests/install_packages.sh
+++ tests/install_packages.sh
@@ -3,8 +3,12 @@ pip install -r tests/requirements.txt
 
 # add chrome browser to sources and install
 if cat tests/requirements.txt | grep -q seleniumbase; then
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-  echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list && \
+  apt-get install -y \
+    gpg && \
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | \
+    gpg --dearmor | \
+    tee /usr/share/keyrings/google.gpg > /dev/null && \
+  echo "deb [signed-by=/usr/share/keyrings/google.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list && \
   apt-get update && apt-get install -y \
   google-chrome-stable && \
   rm -rf /var/lib/apt/lists/* && \
