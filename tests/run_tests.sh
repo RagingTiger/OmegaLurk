2b1028780ca517ae57bbdb3a9b4b1df536c4e913
RagingTiger <yourmom@example.com>
2022-07-30 12:49:53 -0400

    Adding mypy static type checking

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,7 @@
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics && \
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics && \
+printf ">>>>>>>> Starting flake8 Format Checking <<<<<<<<\n\n"
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+printf "\n\n>>>>>>>> Starting MyPy Static Type Checking <<<<<<<<\n\n"
+mypy .
+printf "\n\n>>>>>>>> Starting pytest Testing <<<<<<<<\n\n"
 pytest --cov . --cov-config tests/.coveragerc
