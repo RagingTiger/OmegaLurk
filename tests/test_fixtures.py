2f6830b1a253d0ad4ed6bcf454900846d5a18240
RagingTiger <yourmom@example.com>
2022-08-01 19:55:09 -0400

    Created test file just for streamlit server

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import requests
 
 
 def test_app_path_exists(app_path):
@@ -10,17 +9,3 @@ def test_app_path_exists(app_path):
 
     # check if file
     assert os.path.isfile(app_path)
-
-
-def test_server_up(streamlit_server):
-    """Simple test to see if Streamlit Server is up."""
-    assert streamlit_server.getinfo('streamlit_app').isrunning()
-
-
-def test_server_reachable(streamlit_server):
-    """Test if Streamlit server can be reached at http://localhost:8501."""
-    # send streamlit server GET request
-    response = requests.get('http://localhost:8501')
-
-    # confirm 200 status code
-    assert response.status_code == 200
